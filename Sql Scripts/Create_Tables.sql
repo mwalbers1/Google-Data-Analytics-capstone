36921892dd83e1004cfecb722c0d231870c81673
mwalbers1 <maew711@gmail.com>
2022-06-01 16:57:19 -0400

    feat: Add Create Tables SQL script
    New SQL Scripts folder and Create Tables script

---
+++ Sql Scripts/Create_Tables.sql
@@ -0,0 +1,24 @@
+
+-- Table: public.WEIGHT_LOG_INFO
+
+-- DROP TABLE IF EXISTS public."WEIGHT_LOG_INFO";
+
+CREATE TABLE IF NOT EXISTS public."WEIGHT_LOG_INFO"
+(
+    "ID" bigint,
+    "DATE" timestamp,
+    "WEIGHT_KG" numeric(8,2),
+    "WEIGHT_LBS" numeric(8,2),
+    "FAT" character varying COLLATE pg_catalog."default",
+    "BMI" numeric(8,2),
+    "IS_MANUAL" boolean,
+    "LOG_ID" bigint
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."WEIGHT_LOG_INFO"
+    OWNER to postgres;
+
+GO
+
