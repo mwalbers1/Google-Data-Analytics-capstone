7ee644fc1bc7318c953ee3e54550de4701f4ef8b
mwalbers1 <maew711@gmail.com>
2022-07-02 22:06:32 -0400

    refactor: Remove day name from select
    Day name is no longer needed in result set

--- Sql Scripts/Aggregate_Sleep_Activity.sql
+++ Sql Scripts/Aggregate_Sleep_Activity.sql
@@ -1,14 +1,8 @@
 -- Aggregate_Sleep_Activity.sql
 --
 
--- Calculate average number of hours of sleep for each day of the week
-SELECT DATE_PART('DAY', "SLEEP_DAY"),
-    AVG("TOTAL_MINUTES_ASLEEP") / 60 AS AVERAGE_SLEEP_HOURS
-FROM public."SLEEP_DAY_LOG"
-GROUP BY DATE_PART('DAY', "SLEEP_DAY")
-ORDER BY DATE_PART('DAY', "SLEEP_DAY")
-
--- TODO:
--- sum sleep records for each day by id and add week day column
--- total sleep by day and member
--- then join to DAILY_ACTIVITY table
+-- Hours of sleep for each user by day
+SELECT "ID",
+	"SLEEP_DAY",
+	"TOTAL_MINUTES_ASLEEP"/60 AS SLEEP_HOURS
+FROM public."SLEEP_DAY_LOG";
